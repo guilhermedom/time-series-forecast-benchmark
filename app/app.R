82084a36a5273a68df97d30821420751d4906df0
guilhermedom <guilhermedfs@gmail.com>
2022-08-18 23:11:32 -0300

    style: wrap code around 80 characters per line

--- app/app.R
+++ app/app.R
@@ -2,7 +2,6 @@ library(shiny)
 library(shinythemes)
 library(forecast)
 library(ggplot2)
-library(xts)
 
 # Define UI for application
 ui = fluidPage(
@@ -15,31 +14,37 @@ ui = fluidPage(
     br(),
 
     fluidRow(
-        column(
-            3, align = "center",
-            fileInput("fileInputID", "Choose file to load time series:",
-                      accept = c("text/csv", "text/comma-separated-values", "text/plain", ".csv")),
-            helpText("Note: file must have only one column, without header. Time series frequency must be in months.")
+        column(3, align = "center",
+               fileInput("fileInputID", "Choose file to load time series:",
+                         accept = c("text/csv", "text/comma-separated-values",
+                                    "text/plain", ".csv")),
+               helpText("Note: file must have only one column, without header.
+                        Time series frequency must be in months.")
         ),
-        column(
-            3, align = "center",
-            dateRangeInput("dateRangeInputID", "Time series data range:",
-                           format = "mm/yyyy", start = "2000/01/01", end = "2013/12/31",
+        column(3, align = "center",
+            dateRangeInput("dateRangeInputID", "Time series data range:", format = "mm/yyyy",
+                           start = "2000/01/01", end = "2013/12/31",
                            startview = "year", separator = " to "),
             helpText("Note: you may select any day when defining month and year.")
         ),
-        column(
-            3, align = "center",
-            selectInput("modelSelectionID", "What models do you want to see in the plot?", multiple = T,
-                        choices = c("Naive Forecasting" = "modelNaive", "Mean Forecast" = "modelMeanf", "Random Walk Forecast with Drift" = "modelRwf", "Holt's Linear" = "modelHolt",
-                                    "Holt-Winters Additive" = "modelHw", "Holt-Winters Multiplicative" = "modelHw2",
-                                    "Holt-Winters Multiplicative with Drift" = "modelHw3", "ARIMA" = "modelArima",
-                                    "Linear Model" = "modelTslm", "Neural Network" = "modelNnetar")),
+        column(3, align = "center",
+               selectInput("modelSelectionID", "What models do you want to see in the plot?",
+                           multiple = T,
+                           choices = c("Naive Forecasting" = "modelNaive",
+                                       "Mean Forecast" = "modelMeanf",
+                                       "Random Walk Forecast with Drift" = "modelRwf",
+                                       "Holt's Linear" = "modelHolt",
+                                       "Holt-Winters Additive" = "modelHw",
+                                       "Holt-Winters Multiplicative" = "modelHw2",
+                                       "Holt-Winters Multiplicative with Drift" = "modelHw3",
+                                       "ARIMA" = "modelArima",
+                                       "Linear Model" = "modelTslm",
+                                       "Neural Network" = "modelNnetar")),
         ),
-        column(
-            3, align = "center",
-            numericInput("forecastPeriodID", "How many months do you want to forecast?", 24, min = 1, max = 48),
-            actionButton("forecastButtonID", "Forecast!")
+        column(3, align = "center",
+               numericInput("forecastPeriodID", "How many months do you want to forecast?", 
+                            value = 24, min = 1, max = 48),
+               actionButton("forecastButtonID", "Forecast!")
         )
     ),
     
@@ -90,12 +95,15 @@ server = function(input, output) {
         endYear = as.integer(substr(input$dateRangeInputID[2], 1, 4))
         endMonth = as.integer(substr(input$dateRangeInputID[2], 6, 7))
         
-        timeSeries = ts(data, start = c(startYear, startMonth), end = c(endYear, endMonth), frequency = 12)
+        timeSeries = ts(data, start = c(startYear, startMonth),
+                        end = c(endYear, endMonth), frequency = 12)
         
         forecastPeriod = input$forecastPeriodID
         
-        trainData = window(timeSeries, start = c(startYear, startMonth), end = c(endYear - 2, endMonth))
-        testData = window(timeSeries, start = c(endYear - 2, startMonth), end = c(endYear, endMonth))
+        trainData = window(timeSeries, start = c(startYear, startMonth),
+                           end = c(endYear - 2, endMonth))
+        testData = window(timeSeries, start = c(endYear - 2, startMonth),
+                          end = c(endYear, endMonth))
         
         # naive forecasting
         modelNaive = naive(trainData, h = forecastPeriod)
@@ -108,7 +116,8 @@ server = function(input, output) {
         output$tableMeanfID = renderTable({accuracy(testData, modelMeanf$mean)})
         
         # drift
-        modelRwf = rwf(trainData, h = forecastPeriod, drift = T)
+        modelRwf = rwf(trainData, h = forecastPeriod,
+                       drift = T)
         output$textRwfID = renderText({"Random Walk Forecast with Drift"})
         output$tableRwfID = renderTable({accuracy(testData, modelRwf$mean)})
         
@@ -128,7 +137,8 @@ server = function(input, output) {
         output$tableHw2ID = renderTable({accuracy(testData, modelHw2$mean)})
         
         # Holt-Winters multiplicative with drift
-        modelHw3 = hw(trainData, seasonal = "multiplicative", h = forecastPeriod, damped = T, phi = 0.9)
+        modelHw3 = hw(trainData, seasonal = "multiplicative", h = forecastPeriod,
+                      damped = T, phi = 0.9)
         output$textHw3ID = renderText({"Holt-Winters Multiplicative with Drift"})
         output$tableHw3ID = renderTable({accuracy(testData, modelHw3$mean)})
         
@@ -155,20 +165,31 @@ server = function(input, output) {
             plot(timeSeries, main = "Forecast Benchmark")
             
             mList = input$modelSelectionID
-            if ("modelNaive" %in% mList) lines(modelNaive$mean, pch = 22, lty = 6, col = "red", lwd = 4)
-            if ("modelMeanf" %in% mList) lines(modelMeanf$mean, pch = 22, lty = 5, col = "blue", lwd = 4)
-            if ("modelRwf" %in% mList) lines(modelRwf$mean, pch = 22, lty = 4, col = "green", lwd = 4)
-            if ("modelHolt" %in% mList) lines(modelHolt$mean, pch = 22, lty = 6, col = "cyan", lwd = 4)
-            if ("modelHw" %in% mList) lines(modelHw$mean, pch = 22, lty = 2, col = "black", lwd = 4)
-            if ("modelHw2" %in% mList) lines(modelHw2$mean, pch = 22, lty = 1, col = "purple", lwd = 4)
-            if ("modelHw3" %in% mList) lines(modelHw3$mean, pch = 22, lty = 6, col = "orangered3", lwd = 4)
-            if ("modelArima" %in% mList) lines(modelArima$mean, pch = 22, lty = 5, col = "gold", lwd = 4)
-            if ("modelTslm" %in% mList) lines(modelTslm$mean, pch = 22, lty = 4, col = "chocolate1", lwd = 4)
-            if ("modelNnetar" %in% mList) lines(modelNnetar$mean, pch = 22, lty = 6, col = "magenta", lwd = 4)
+            if ("modelNaive" %in% mList) 
+                lines(modelNaive$mean, pch = 22, lty = 6, col = "red", lwd = 4)
+            if ("modelMeanf" %in% mList) 
+                lines(modelMeanf$mean, pch = 22, lty = 5, col = "blue", lwd = 4)
+            if ("modelRwf" %in% mList) 
+                lines(modelRwf$mean, pch = 22, lty = 4, col = "green", lwd = 4)
+            if ("modelHolt" %in% mList) 
+                lines(modelHolt$mean, pch = 22, lty = 6, col = "cyan", lwd = 4)
+            if ("modelHw" %in% mList) 
+                lines(modelHw$mean, pch = 22, lty = 2, col = "black", lwd = 4)
+            if ("modelHw2" %in% mList) 
+                lines(modelHw2$mean, pch = 22, lty = 1, col = "purple", lwd = 4)
+            if ("modelHw3" %in% mList) 
+                lines(modelHw3$mean, pch = 22, lty = 6, col = "orangered3", lwd = 4)
+            if ("modelArima" %in% mList) 
+                lines(modelArima$mean, pch = 22, lty = 5, col = "gold", lwd = 4)
+            if ("modelTslm" %in% mList) 
+                lines(modelTslm$mean, pch = 22, lty = 4, col = "chocolate1", lwd = 4)
+            if ("modelNnetar" %in% mList) 
+                lines(modelNnetar$mean, pch = 22, lty = 6, col = "magenta", lwd = 4)
             
             legend("topleft", 
-                   legend = c("Naive", "Mean", "RWF Drift", "Holt's Linear", "HW Add.", "HW Mult.",
-                              "HW Mult. Drift", "ARIMA", "Linear Model", "Neural Network"),
+                   legend = c("Naive", "Mean", "RWF Drift", "Holt's Linear",
+                              "HW Add.", "HW Mult.", "HW Mult. Drift", "ARIMA",
+                              "Linear Model", "Neural Network"),
                    col = c("red", "blue", "green", "cyan", "black",
                            "purple", "orangered3", "gold", "chocolate1", "magenta"),
                    cex = 1.4, ncol = 4, lwd = 7
